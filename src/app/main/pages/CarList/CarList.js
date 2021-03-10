17f9301941840abdfebc85133c794dd16fac8d78
jeffrey1226 <wfhupwork26@gmail.com>
2021-03-10 11:46:03 +0800

    First commit

---
+++ src/app/main/pages/CarList/CarList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CarList extends Component {
+  render() {
+    return (
+      <div className="container flex justify-center items-center h-64 text-white h-full">
+        <h2>List Component</h2>
+      </div>
+    );
+  }
+}
